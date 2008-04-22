085151c54470cf0d60934beb0e07ebb3fc2c985d
nesher_cp <SND\nesher_cp>
2008-04-22 20:14:30 +0000

    Contracts for InputProviders

---
+++ Main Source/Multitouch.Contracts/ContactState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Multitouch.Contracts.Contracts
+{
+	public enum ContactState
+	{
+		/// <summary>
+		/// A new contact
+		/// </summary>
+		New,
+		/// <summary>
+		/// Contact removed
+		/// </summary>
+		Removed,
+		/// <summary>
+		/// Contact moved
+		/// </summary>
+		Moved
+	}
+}
---
+++ Main Source/Multitouch.Contracts/IProviderContract.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.AddIn.Contract;
+using System.AddIn.Pipeline;
+using PipelineHints;
+
+[assembly:SegmentAssemblyName(PipelineSegment.AddInSideAdapter, "Multitouch.AddInSideAdapter")]
+[assembly:SegmentAssemblyName(PipelineSegment.AddInView, "Multitouch.AddInView")]
+[assembly:SegmentAssemblyName(PipelineSegment.HostSideAdapter, "Multitouch.HostSideAdapter")]
+[assembly:SegmentAssemblyName(PipelineSegment.HostView, "Multitouch.HostView")]
+
+namespace Multitouch.Contracts.Contracts
+{
+	[AddInContract]
+	public interface IProviderContract : IContract
+	{
+		[Comment(
+		"/// <summary>"+
+		"/// Starts provider and sending events."+
+		"/// </summary>"
+		)]
+		void Start();
+		[Comment(
+		"/// <summary>"+
+		"/// Stops provider and sending events."+
+		"/// </summary>"
+		)]
+		void Stop();
+		[Comment(
+		"/// <summary>"+
+		"/// Returns <c>True</c> if provider is running, else <c>False</c>."+
+		"/// </summary>"
+		)]
+		bool IsRunning { get; }
+
+		[Comment(
+		"/// <summary>"+
+		"/// Raised when contact changes."+
+		"/// </summary>"
+		)]
+		[EventAdd("ContactChanged")]
+		void ContactChangedAdd(IContactChangedEventHandlerContract handler);
+		[EventRemove("ContactChanged")]
+		void ContactChangedRemove(IContactChangedEventHandlerContract handler);
+	}
+}
