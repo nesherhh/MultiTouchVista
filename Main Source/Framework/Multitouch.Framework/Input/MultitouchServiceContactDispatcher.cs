1c5b22d70d694211a1f2ea98be7cc51ede9efa48
nesher_cp <SND\nesher_cp>
2008-04-22 20:25:04 +0000

    Framework is a general project that communicates with MTSvc. Framework.WPF is a specification for WPF and includes controls.

---
+++ Main Source/Framework/Multitouch.Framework/Input/MultitouchServiceContactDispatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Multitouch.Framework.Input
+{
+	class MultitouchServiceContactDispatcher : IApplicationInterfaceCallback
+	{
+		public MultitouchServiceContactDispatcher(MultitouchLogic logic)
+		{
+		}
+
+		public void ContactChanged(int id, double x, double y, double width, double height, ContactState state)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Main Source/Framework/Multitouch.Framework/Input/ServiceCommunicator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceModel;
+using System.Windows;
+
+namespace Multitouch.Framework.Input
+{
+	class ServiceCommunicator
+	{
+		MultitouchLogic logic;
+		ApplicationInterfaceClient client;
+        IApplicationInterfaceCallback contactDispatcher;
+
+		public ServiceCommunicator(MultitouchLogic logic)
+		{
+			this.logic = logic;
+			Application.Current.Exit += Current_Exit;
+		}
+
+		void Current_Exit(object sender, ExitEventArgs e)
+		{
+			Disconnect();
+		}
+
+		public void Connect()
+		{
+			Uri serviceAddress = new Uri("net.pipe://localhost/Multitouch.Service/ApplicationInterface");
+			EndpointAddress remoteAddress = new EndpointAddress(serviceAddress);
+			NetNamedPipeBinding namedPipeBinding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+
+			IApplicationInterfaceCallback dispatcher = new MultitouchServiceContactDispatcher(logic);
+			InstanceContext instanceContext = new InstanceContext(dispatcher);
+			client = new ApplicationInterfaceClient(instanceContext, namedPipeBinding, remoteAddress);
+
+			try
+			{
+				client.Subscribe();
+				contactDispatcher = dispatcher;
+			}
+			catch (EndpointNotFoundException)
+			{
+				Trace.TraceWarning("Could not connect to Multitouch service, falling back to single touch mouse input");
+				contactDispatcher = new SingleMouseContactDispatcher(logic);
+			}
+		}
+
+		void Disconnect()
+		{
+			if(contactDispatcher != null)
+			{
+				client.Unsubscribe();
+                client.Close();
+				contactDispatcher = null;
+				client = null;
+			}
+		}
+	}
+}
