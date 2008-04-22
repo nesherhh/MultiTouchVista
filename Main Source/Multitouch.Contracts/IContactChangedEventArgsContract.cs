2b76a7f2892280aa3ead70631fe212936b56771b
nesher_cp <nesher_hh@msn.com>
2008-04-22 20:14:30 +0000

    Contracts for InputProviders

---
+++ Main Source/Multitouch.Contracts/IContactChangedEventArgsContract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.AddIn.Contract;
+using PipelineHints;
+
+namespace Multitouch.Contracts.Contracts
+{
+	[EventArgs]
+	public interface IContactChangedEventArgsContract : IContract
+	{
+		[Comment(
+		"/// <summary>"+
+		"/// Changed contact."+
+		"/// </summary>"
+		)]
+		IContactContract Contact { get; }
+	}
+}
---
+++ Main Source/Multitouch.Contracts/IContactChangedEventHandlerContract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.AddIn.Contract;
+using PipelineHints;
+
+namespace Multitouch.Contracts.Contracts
+{
+	[EventHandler]
+	public interface IContactChangedEventHandlerContract : IContract
+	{
+		void Handler(IContactChangedEventArgsContract args);
+	}
+}
