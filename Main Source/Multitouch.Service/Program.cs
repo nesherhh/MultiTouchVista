e1322e90b1629d5aefd73e916de36c1598d35ab5
Pleh_cp <SND\Pleh_cp>
2008-04-23 10:47:01 +0000

    Added Tray Icon Functionality, Debug Console Window and Blank Options form. There are some threadding/imput issues that I think are caused my the MultiMouse RawInput provider.

--- Main Source/Multitouch.Service/Program.cs
+++ Main Source/Multitouch.Service/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace Multitouch.Service
 {
@@ -16,13 +17,55 @@ namespace Multitouch.Service
 			//};
 			//ServiceBase.Run(ServicesToRun);
 
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			debug = new frmDebugConsole();
+
+			// 
+			// contextMenu
+			//
+			ContextMenuStrip contextMenu = new ContextMenuStrip();
+			contextMenu.Items.Add("Debug Console", null, Debug_Click);
+			contextMenu.Items.Add("Options", null, Options_Click);
+			contextMenu.Items.Add(new ToolStripSeparator());
+			contextMenu.Items.Add("Exit", null, Exit_Click);
+			// 
+			// notifyIcon
+			// 
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOptions));
+			NotifyIcon notifyIcon = new NotifyIcon();
+			notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			notifyIcon.Text = "MultiTouch Service";
+			notifyIcon.ContextMenuStrip = contextMenu;
+			notifyIcon.Visible = true;
 
 			MultitouchInput input = new MultitouchInput();
-			input.Start();
+			//input.Start(); //For some reason starting the input prevents context menu mouse events
 			Console.WriteLine("Multitouch service is running.");
-			Console.WriteLine("Press ENTER to stop and exit.");
-			Console.ReadLine();
+			Application.Run();
+			notifyIcon.Visible = false;
 			input.Stop();
+
 		}
+
+		static frmDebugConsole debug;
+		static void Debug_Click(Object sender, EventArgs e)
+		{
+			debug.Show();
+		}
+
+		static void Options_Click(Object sender, EventArgs e)
+		{
+			frmOptions options = new frmOptions();
+			options.Show();
+		}
+
+		static void Exit_Click(Object sender, EventArgs e)
+		{
+			// Manualy dispose the debug console because it is set to cancel standard form closing
+			debug.Dispose();
+			Application.Exit();
+		}
+
 	}
 }
