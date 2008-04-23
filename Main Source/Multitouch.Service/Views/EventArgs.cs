ea1861a86bff46f27a110c6a48747331e3cab39f
nesher_cp <SND\nesher_cp>
2008-04-23 20:39:40 +0000

    Made some refactorings. Actualy I'm not happy with GUI in Service. Do we need to use NUI logo? :S

---
+++ Main Source/Multitouch.Service/Views/EventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Multitouch.Service.Views
+{
+	class EventArgs<T>:EventArgs
+	{
+		public T Item { get; private set; }
+
+		public EventArgs(T item)
+		{
+			Item = item;
+		}
+	}
+}
