a89845f9558a500f6b6ef435d3cc8594188fa8a3
Pleh_cp <SND\Pleh_cp>
2008-04-24 08:34:57 +0000

    Created New Project for Singletouch Service

---
+++ Main Source/Singletouch.Service/SingletouchService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Singletouch.Service
+{
+	public partial class SingletouchService : ServiceBase
+	{
+		public SingletouchService()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
