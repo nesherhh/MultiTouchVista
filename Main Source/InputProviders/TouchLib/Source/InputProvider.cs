dfeb3d9942efab05028ebe4f6115e2a38c579b86
nesher_cp <nesher_hh@msn.com>
2008-05-03 18:56:23 +0000

    Updated TouchLib input provider.

--- Main Source/InputProviders/TouchLib/Source/InputProvider.cs
+++ Main Source/InputProviders/TouchLib/Source/InputProvider.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.AddIn;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
 using Multitouch.Contracts;
 using TouchlibWrapper;
 
@@ -9,22 +12,43 @@ namespace TouchLib
 	public class InputProvider : IProvider
 	{
 		CsTI instance;
+		int screenWidth;
+		int screenHeight;
 		internal const string VERSION = "1.0.0.0";
 
 		public event EventHandler<ContactChangedEventArgs> ContactChanged;
 
 		public void Start()
 		{
+			screenWidth = Screen.PrimaryScreen.Bounds.Width;
+			screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
 			instance = CsTI.Instance();
 			instance.fingerDown += new fingerDownHandler(instance_fingerDown);
 			instance.fingerUp += new fingerUpHandler(instance_fingerUp);
 			instance.fingerUpdate += new fingerUpdateHandler(instance_fingerUpdate);
-			instance.startScreen();
-			IsRunning = true;
+
+
+			Thread t = new Thread(ThreadWorker);
+			t.SetApartmentState(ApartmentState.MTA);
+			t.IsBackground = true;
+			t.Name = "touchlib thread";
+			t.Start();
+		}
+
+		void ThreadWorker()
+		{
+			if (instance != null)
+			{
+				IsRunning = true;
+				instance.startScreen(true);
+			}
 		}
 
 		public void Stop()
 		{
+			if(instance != null)
+				instance.stopScreen();
 			instance = null;
 			IsRunning = false;
 		}
@@ -39,17 +63,23 @@ namespace TouchLib
 
 		void instance_fingerUpdate(int id, int tagID, float x, float y, float angle, float area, float height, float width, float dX, float dY)
 		{
+			x = x * screenWidth;
+			y = y * screenHeight;
 			OnContactChanged(new TouchLibContactChangedEventArgs(new TouchLibContact(id, x, y, width, height, ContactState.Moved)));
 		}
 
 		void instance_fingerUp(int id, int tagID, float x, float y, float angle, float area, float height, float width, float dX, float dY)
 		{
-			OnContactChanged(new TouchLibContactChangedEventArgs(new TouchLibContact(id, x, y, width, height, ContactState.New)));
+			x = x * screenWidth;
+			y = y * screenHeight;
+			OnContactChanged(new TouchLibContactChangedEventArgs(new TouchLibContact(id, x, y, width, height, ContactState.Removed)));
 		}
 
 		void instance_fingerDown(int id, int tagID, float x, float y, float angle, float area, float height, float width, float dX, float dY)
 		{
-			OnContactChanged(new TouchLibContactChangedEventArgs(new TouchLibContact(id, x, y, width, height, ContactState.Removed)));
+			x = x * screenWidth;
+			y = y * screenHeight;
+			OnContactChanged(new TouchLibContactChangedEventArgs(new TouchLibContact(id, x, y, width, height, ContactState.New)));
 		}
 	}
 }
