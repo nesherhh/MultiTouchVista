44d054a9d911acb705705a26c90b4a0a03dde02e
nesher_cp <SND\nesher_cp>
2008-05-17 20:18:15 +0000

    Gestures

---
+++ Main Source/Framework/Multitouch.Framework.WPF/Input/GestureEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Ink;
+
+namespace Multitouch.Framework.WPF.Input
+{
+	public class GestureEventArgs : ContactEventArgs
+	{
+		public ApplicationGesture Gesture { get; private set; }
+
+		internal GestureEventArgs(RawMultitouchGestureReport raw, int timestamp)
+			: base(raw.MultitouchDevice, raw, timestamp)
+		{
+			Gesture = raw.Gesture;
+		}
+
+		public GestureEventArgs(GestureEventArgs copy, int timestamp)
+			: base(copy.MultitouchDevice, copy.Contact, timestamp)
+		{
+			Gesture = copy.Gesture;
+		}
+
+		protected override void InvokeEventHandler(Delegate genericHandler, object genericTarget)
+		{
+			GestureEventHandler handler = (GestureEventHandler)genericHandler;
+			handler(genericTarget, this);
+		}
+	}
+}
