3e0ff983cde20a71f377b5762100b8ed42c2e4bc
nesher_cp <nesher_hh@msn.com>
2008-06-22 20:42:02 +0000

    DWMaxx Addin can now also scale windows

---
+++ Main Source/Multitouch.Service.AddIns/DWMaxxAddIn/WindowContacts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace DWMaxxAddIn
+{
+	class WindowContacts : KeyedCollection<int, WindowContact>
+	{
+		readonly Window owner;
+
+		public WindowContacts(Window owner)
+		{
+			this.owner = owner;
+		}
+
+		protected override int GetKeyForItem(WindowContact item)
+		{
+			return item.Id;
+		}
+
+		protected override void InsertItem(int index, WindowContact item)
+		{
+			item.SetOwner(owner);
+			base.InsertItem(index, item);
+		}
+
+		public bool TryGetValue(int id, out WindowContact contact)
+		{
+			if (Contains(id))
+			{
+				contact = this[id];
+				return true;
+			}
+			contact = null;
+			return false;
+		}
+	}
+}
