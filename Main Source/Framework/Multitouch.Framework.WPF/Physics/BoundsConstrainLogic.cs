a259238f4e5b4bfa3aec0d051bfb70fd62acec2d
nesher_cp <nesher_hh@msn.com>
2008-06-28 17:41:22 +0000

    ScrollViewer updated

---
+++ Main Source/Framework/Multitouch.Framework.WPF/Physics/BoundsConstrainLogic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using AdvanceMath;
+using Multitouch.Framework.WPF.Controls;
+using Physics2DDotNet;
+using Physics2DDotNet.PhysicsLogics;
+
+namespace Multitouch.Framework.WPF.Physics
+{
+	class BoundsConstrainLogic : PhysicsLogic
+	{
+		readonly ScrollViewer viewer;
+
+		public BoundsConstrainLogic(ScrollViewer viewer)
+			: base(new Lifespan())
+		{
+			this.viewer = viewer;
+		}
+
+		protected override void RunLogic(TimeStep step)
+		{
+			Vector2D linear = -viewer.Body.State.Position.Linear;
+			Vector2D velocity = -viewer.Body.State.Velocity.Linear;
+			if (linear.X < 0)
+			{
+				linear.X = 0;
+				velocity.X = 0;
+			}
+			else if (viewer.ScrollableWidth > 0 && linear.X > viewer.ScrollableWidth)
+			{
+				linear.X = viewer.ScrollableWidth;
+				velocity.X = 0;
+			}
+			if (linear.Y < 0)
+			{
+				linear.Y = 0;
+				velocity.Y = 0;
+			}
+			else if (viewer.ScrollableHeight > 0 && linear.Y > viewer.ScrollableHeight)
+			{
+				linear.Y = viewer.ScrollableHeight;
+				velocity.Y = 0;
+			}
+			viewer.Body.State.Position.Linear = -linear;
+			viewer.Body.State.Velocity.Linear = -velocity;
+		}
+	}
+}
---
+++ Main Source/Framework/Multitouch.Framework.WPF/Physics/FixedSlidingHingeJoint.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using AdvanceMath;
+using Physics2DDotNet;
+using Physics2DDotNet.Joints;
+using Physics2DDotNet.Solvers;
+using Scalar = System.Double;
+
+namespace Multitouch.Framework.WPF.Physics
+{
+	/// <summary>
+	/// A joint that makes a single Body Pivot around an Anchor.
+	/// </summary>
+	[Serializable]
+	public sealed class FixedSlidingHingeJoint : Joint, ISequentialImpulsesJoint
+	{
+		SequentialImpulsesSolver solver;
+		Body body;
+
+
+		Matrix2x2 M;
+		Vector2D localAnchor1;
+		Vector2D anchor;
+		Vector2D r1;
+		Vector2D bias;
+		Vector2D accumulatedImpulse;
+		Scalar biasFactor;
+		Scalar softness;
+		Scalar distanceTolerance;
+
+
+		public FixedSlidingHingeJoint(Body body, Vector2D anchor, Lifespan lifetime, Orientation orientation)
+			: base(lifetime)
+		{
+			if (body == null) { throw new ArgumentNullException("body"); }
+			this.body = body;
+			this.anchor = anchor;
+			Orientation = orientation;
+			body.ApplyPosition();
+			Vector2D.Transform(ref body.Matrices.ToBody, ref anchor, out localAnchor1);
+			softness = 0.001f;
+			biasFactor = 0.2f;
+			distanceTolerance = Scalar.PositiveInfinity;
+		}
+
+		Orientation Orientation { get; set; }
+
+		public Vector2D Anchor
+		{
+			get { return anchor; }
+			set { anchor = value; }
+		}
+
+		public Scalar BiasFactor
+		{
+			get { return biasFactor; }
+			set { biasFactor = value; }
+		}
+		public Scalar Softness
+		{
+			get { return softness; }
+			set { softness = value; }
+		}
+		/// <summary>
+		/// The distance the joint can stretch before breaking. 
+		/// </summary>
+		public Scalar DistanceTolerance
+		{
+			get { return distanceTolerance; }
+			set
+			{
+				if (value <= 0) { throw new ArgumentOutOfRangeException("value"); }
+				distanceTolerance = value;
+			}
+		}
+		public override ReadOnlyCollection<Body> Bodies
+		{
+			get { return new ReadOnlyCollection<Body>(new[] { body }); }
+		}
+		protected override void OnAdded(EventArgs e)
+		{
+			solver = (SequentialImpulsesSolver)Engine.Solver;
+			base.OnAdded(e);
+		}
+		void ISequentialImpulsesJoint.PreStep(TimeStep step)
+		{
+
+			Scalar mass1Inv = body.Mass.MassInv;
+			Scalar inertia1Inv = body.Mass.MomentOfInertiaInv;
+
+			// Pre-compute anchors, mass matrix, and bias.
+
+			Vector2D.TransformNormal(ref body.Matrices.ToWorld, ref localAnchor1, out r1);
+
+			// deltaV = deltaV0 + K * impulse
+			// invM = [(1/m1 + 1/m2) * eye(2) - skew(r1) * invI1 * skew(r1) - skew(r2) * invI2 * skew(r2)]
+			//      = [1/m1+1/m2     0    ] + invI1 * [r1.y*r1.y -r1.x*r1.y] + invI2 * [r1.y*r1.y -r1.x*r1.y]
+			//        [    0     1/m1+1/m2]           [-r1.x*r1.y r1.x*r1.x]           [-r1.x*r1.y r1.x*r1.x]
+
+			Matrix2x2 K;
+			K.m00 = mass1Inv;
+			K.m11 = mass1Inv;
+
+			K.m00 += inertia1Inv * r1.Y * r1.Y;
+			K.m01 = -inertia1Inv * r1.X * r1.Y;
+			K.m10 = -inertia1Inv * r1.X * r1.Y;
+			K.m11 += inertia1Inv * r1.X * r1.X;
+
+			K.m00 += softness;
+			K.m11 += softness;
+
+			Matrix2x2.Invert(ref K, out M);
+
+
+			Vector2D dp;
+			Vector2D.Add(ref body.State.Position.Linear, ref r1, out dp);
+			Vector2D.Subtract(ref anchor, ref dp, out dp);
+
+			if (!Scalar.IsPositiveInfinity(distanceTolerance) &&
+			    dp.MagnitudeSq > distanceTolerance * distanceTolerance)
+			{
+				Lifetime.IsExpired = true;
+			}
+
+			if (solver.PositionCorrection)
+			{
+				//bias = -0.1f * dtInv * dp;
+				Scalar flt = -biasFactor * step.DtInv;
+				Vector2D.Multiply(ref dp, ref flt, out bias);
+			}
+			else
+			{
+				bias = Vector2D.Zero;
+			}
+			if (solver.WarmStarting)
+			{
+				PhysicsHelper.SubtractImpulse(
+					ref body.State.Velocity, ref accumulatedImpulse,
+					ref r1, ref mass1Inv, ref inertia1Inv);
+			}
+			else
+			{
+				accumulatedImpulse = Vector2D.Zero;
+			}
+			body.ApplyProxy();
+
+		}
+
+		void ISequentialImpulsesJoint.ApplyImpulse()
+		{
+			Scalar mass1Inv = body.Mass.MassInv;
+			Scalar inertia1Inv = body.Mass.MomentOfInertiaInv;
+
+			Vector2D dv;
+			PhysicsHelper.GetRelativeVelocity(ref body.State.Velocity, ref r1, out dv);
+
+
+			Vector2D impulse = new Vector2D();
+			if(Orientation == Orientation.Horizontal)
+				impulse.X = bias.X - dv.X - softness * accumulatedImpulse.X;
+			if(Orientation == Orientation.Vertical)
+				impulse.Y = bias.Y - dv.Y - softness * accumulatedImpulse.Y;
+			Vector2D.Transform(ref  M, ref impulse, out impulse);
+			//impulse = M * (bias - dv - softness * P);
+
+
+			PhysicsHelper.SubtractImpulse(
+				ref body.State.Velocity, ref impulse,
+				ref r1, ref mass1Inv, ref inertia1Inv);
+
+
+			Vector2D.Add(ref accumulatedImpulse, ref impulse, out accumulatedImpulse);
+			body.ApplyProxy();
+		}
+	}
+}
