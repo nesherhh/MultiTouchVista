a3c7a7c4db0b777799a54719b242df833a53ddcb
nesher_cp <nesher_hh@msn.com>
2008-07-01 19:16:58 +0000

    TUIO input provider

---
+++ Main Source/InputProviders/TuioProvider/TuioContact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Multitouch.Contracts;
+
+namespace TuioProvider
+{
+	class TuioContact : IContact
+	{
+		public TuioContact(int id, double x, double y, double width, double height, ContactState state)
+		{
+			Id = id;
+			X = x;
+			Y = y;
+			Width = width;
+			Height = height;
+			State = state;
+		}
+
+		public int Id { get; private set; }
+		public double X { get; private set; }
+		public double Y { get; private set; }
+		public double Width { get; private set; }
+		public double Height { get; private set; }
+		public ContactState State { get; private set; }
+	}
+}
---
+++ Main Source/InputProviders/TuioProvider/TuioContactChangedEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Multitouch.Contracts;
+using TUIO;
+
+namespace TuioProvider
+{
+	class TuioContactChangedEventArgs : ContactChangedEventArgs
+	{
+		IContact contact;
+		static Size monitorSize;
+
+		static TuioContactChangedEventArgs()
+		{
+			monitorSize = SystemInformation.PrimaryMonitorSize;
+		}
+
+		public TuioContactChangedEventArgs(TuioCursor cursor, ContactState state)
+		{
+			contact = new TuioContact(cursor.getFingerID(), cursor.getX() * monitorSize.Width, cursor.getY() * monitorSize.Height, 10, 10, state);
+		}
+
+		public override IContact Contact
+		{
+			get { return contact; }
+		}
+	}
+}
